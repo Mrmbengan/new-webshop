cdffbc3ead810a0e32a0f20bd9194c2417829fd9
Matias Bengtsson <bengtsson1985@hotmail.com>
2023-10-11 21:01:31 +0200

    updating directory

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Directory from './components/directory/directory.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './categories.styles.scss';
+
+const App = () => { 
+
+
+  const categories = [
+    
+      {
+        "id": 1,
+        "title": "hats",
+        "imageUrl": "https://images.unsplash.com/photo-1572994631587-b3b33b849ebd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
+      },
+      {
+        "id": 2,
+        "title": "jackets",
+        "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+      },
+      {
+        "id": 3,
+        "title": "sneakers",
+        "imageUrl": "https://images.unsplash.com/photo-1655853548169-646b6e0f15ca?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
+      },
+      {
+        "id": 4,
+        "title": "womens",
+        "imageUrl": "https://images.unsplash.com/photo-1541823709867-1b206113eafd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
+      },
+      {
+        "id": 5,
+        "title": "mens",
+        "imageUrl": "https://images.unsplash.com/photo-1676278746071-cd54ce97bf16?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
+      }
+  ];
+
+  return<Directory categories={categories} />;
+  
+
+  
+};
 
 export default App;
