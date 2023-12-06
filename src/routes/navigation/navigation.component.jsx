13d589f260c5ed916052167a480de4482d01b20d
Matias Bengtsson <bengtsson1985@hotmail.com>
2023-12-06 20:58:53 +0100

    adding cart and shopping and cartdropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,8 +1,11 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { useNavigate } from 'react-router-dom';
 import ButtonImage from '../../assets/MepplClothing2.png';
 import { UserContext } from "../../context/user.context";
+import { CartContext } from "../../context/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import './navigation.styles.scss';
 
@@ -12,11 +15,12 @@ const Navigation = () => {
     navigate('/');
   };
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
     return (
       <Fragment>
         <div className="navigation">
-          <button className='button' onClick={handleButtonClick}>
+          <button className='navbutton' onClick={handleButtonClick}>
             <img src={ButtonImage} alt="Button" />
           </button>
           <div className='nav-links-container'>
@@ -32,7 +36,9 @@ const Navigation = () => {
               Sign In 
             </Link>
             )}
+            <CartIcon />
           </div>
+          {isCartOpen && <CartDropdown/>}
         </div>
         <Outlet />
       </Fragment>
